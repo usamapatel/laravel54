313e4b5be811f5ee4d9bcd9deec623c52d59fabd
Sunshine <sunshine.mahant@gmail.com>
2017-03-08 17:30:52 +0530

    talk

---
+++ config/talk.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'user' => [
+        'model' => 'App\Models\User',
+    ],
+    'broadcast' => [
+        'enable' => false,
+        'app_name' => 'your-app-name',
+        'pusher' => [
+            'app_id' => '',
+            'app_key' => '',
+            'app_secret' => '',
+        ],
+    ],
+];
