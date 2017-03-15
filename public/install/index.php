18fee2d48d7ca37b5cc063377d2ae5b37a465598
Sunshine <sunshine.mahant@gmail.com>
2017-03-15 09:12:30 +0530

    future installation

---
+++ public/install/index.php
@@ -0,0 +1,19 @@
+<?php
+
+set_time_limit(100);
+ini_set('memory_limit', -1);
+
+//TODO! Some Authorization - Whitelisted IP, Security tokens...
+
+echo '<pre>
+   ______
+  / ____/___  ____ ___  ____  ____  ________  _____
+ / /   / __ \/ __ `__ \/ __ \/ __ \/ ___/ _ \/ ___/
+/ /___/ /_/ / / / / / / /_/ / /_/ (__  )  __/ /
+\____/\____/_/ /_/ /_/ .___/\____/____/\___/_/  INSTALL
+                    /_/
+
+Installation script will come on this URL
+';
+
+define('ROOT_DIR', realpath('../'));
