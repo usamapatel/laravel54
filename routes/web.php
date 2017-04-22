1ac0c2dfe64054b104697dfff03616b1f5464ef7
Sunny Sheth <ssheth@aecordigital.com>
2017-04-22 12:36:02 +0530

    permission related changes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,22 +9,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('index');
 })->name('front.index');
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('admin.home');
-
 Route::get('/dashboard', 'DashboardController@index')->name('admin.dashboard');
-
 // Local dev specific routes
 if (App::environment('local')) {
     /*Route::get('decompose', '\Lubusin\Decomposer\Controllers\DecomposerController@index');*/
 }
-
 Route::group(['middleware' => ['auth'], 'prefix' => 'admin'], function () {
 	Route::resource('roles', 'RolesController');
     Route::post('/getRoleData', 'RolesController@getRoleData');
