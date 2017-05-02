6bec25f39fda4fa682545a37995f94c1a65ed398
rishabh <rishabh>
2017-05-02 11:08:37 +0530

    changes

--- resources/views/partial/admin/users/form.blade.php
+++ resources/views/partial/admin/users/form.blade.php
@@ -1,18 +1,18 @@
 <div class="form-body">
     <div class="form-group">
-        <label class="col-md-3 control-label">Name </label>
+        <label class="col-md-2 control-label">Name </label>
         <div class="col-md-9">
             {!! Form::text('name', $from=="edit" ? $user->name : null,['class' => 'form-control']) !!}
         </div>
     </div>
     <div class="form-group">
-        <label class="control-label col-md-3">Email</label>
+        <label class="control-label col-md-2">Email</label>
         <div class="col-md-9">
             {!! Form::email('email', $from=="edit" ? $user->email : null,['class' => 'form-control']) !!}
         </div>
     </div>
     <div class="form-group">
-        <label class="control-label col-md-3">Created at</label>
+        <label class="control-label col-md-2">Created at</label>
         <div class="col-md-9">
             <div class='input-group date js-form-datetimepicker'>
                 {!! Form::text('banned_at', $from=="edit" ? Carbon\Carbon::parse($user->banned_at)->format('d/m/Y h:i A') : null,
@@ -24,11 +24,11 @@
         </div>
     </div>
     <div class="form-group">
-        <label class="col-md-3 control-label">Roles</label>
+        <label class="col-md-2 control-label">Roles</label>
         <div class="col-md-9">
             <div class="row">
                 @foreach($roles as $role)
-                    <div class="col-md-3">
+                    <div class="col-md-2">
                         {{ Form::checkbox('roles[]', $role->name, $from == "edit" ? $user->hasRole($role->name) : null ) }} {{ $role->display_name }}<br>
                     </div>
                 @endforeach
@@ -37,9 +37,9 @@
     </div>
     <div class="form-actions">
         <div class="row">
-            <div class="col-md-offset-3 col-md-9">
+            <div class="col-md-12 text-center">
                 <button type="submit" class="btn green">Submit</button>
-                <a class="btn default" href="{{ route('users.index', ['domain' => app('request')->route()->parameter('company')]) }}">Cancel</a>
+                <a class="btn red-sunglo" href="{{ route('users.index', ['domain' => app('request')->route()->parameter('company')]) }}">Cancel</a>
             </div>
         </div>
     </div>
