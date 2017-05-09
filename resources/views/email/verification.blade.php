248c24e353d882b504e69f632f460f0eae324b0a
Sunshine <sunshine.mahant@gmail.com>
2017-05-10 02:25:39 +0530

    user verification

--- resources/views/email/verification.blade.php
+++ resources/views/email/verification.blade.php
@@ -1,2 +1,2 @@
 <h1>Click the Link To Verify Your Email</h1>
-Click the following link to verify your email {{url('/verifyemail/'.$verification_token)}}
+Click the following link to verify your email <a href="{{url('/verifyemail/'.$verification_token)}}">{{url('/verifyemail/'.$verification_token)}}</a>
