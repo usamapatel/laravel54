cb85afc0a47b6d65f25aa1a050667dc584b0a28a
Mukesh Tilokani <mtilokani@aecordigital.com>
2017-05-10 19:52:14 +0530

    resolved issues and user section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,8 +61,8 @@
                         </li>
                         <!-- Authentication Links -->
                         @if (Auth::guest())
-                            <li><a href="{{ route('login') }}">{{ __("Login") }}</a></li>
-                            <li><a href="{{ route('register') }}">{{ __("Register") }}</a></li>
+                            <li><a href="{{ route('login', ['domain' => app('request')->route()->parameter('company')]) }}">{{ __("Login") }}</a></li>
+                            <li><a href="{{ route('register', ['domain' => app('request')->route()->parameter('company')]) }}">{{ __("Register") }}</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
--- resources/views/partial/admin/users/form.blade.php
+++ resources/views/partial/admin/users/form.blade.php
@@ -1,48 +1,10 @@
 <div class="form-body">
     <div class="form-row col-md-6 clearfix">
         <div class="form-col-1">
-            <label class="label">Name </label>
-        </div>
-        <div class="p-r-5 input-wrapper right">
-            {!! Form::text('first_name', $from=="edit" ? $user->person->first_name : null,['class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="form-row col-md-6 clearfix">
-        <div class="form-col-1">
-            <label class="label">Last Name </label>
-        </div>
-        <div class="p-r-5 input-wrapper right">
-            {!! Form::text('last_name', $from=="edit" ? $user->person->last_name : null,['class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="form-row col-md-6 clearfix">
-        <div class="form-col-1">
-            <label class="label">Username </label>
-        </div>
-        <div class="p-r-5 input-wrapper right">
-            {!! Form::text('username', $from=="edit" ? $user->username : null,['class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="form-row col-md-6 clearfix">
-        <div class="form-col-1">
             <label class="label">Email </label>
         </div>
         <div class="p-r-5 input-wrapper right">
-            {!! Form::email('email', $from=="edit" ? $user->email : null,['class' => 'form-control']) !!}
-        </div>
-    </div>
-    <div class="form-row col-md-6 clearfix">
-        <div class="form-col-1">
-            <label class="label">Created at </label>
-        </div>
-        <div class="p-r-5 input-wrapper right">
-            <div class='input-group date js-form-datetimepicker'>
-                {!! Form::text('banned_at', $from=="edit" ? Carbon\Carbon::parse($user->banned_at)->format('d/m/Y h:i A') : null,
-                ['class' => 'form-control', 'id' => 'banned_at', 'readonly' => 'readonly']) !!}
-                <span class="input-group-addon">
-                    <span class="glyphicon glyphicon-calendar"></span>
-                </span>
-            </div>
+            {!! Form::email('email', $from=="edit" ? $user->email : null,['class' => 'form-control', 'id' => 'email']) !!}
         </div>
     </div>
     <div class="form-row col-md-6 clearfix">
@@ -56,6 +18,46 @@
                         {{ Form::checkbox('roles[]', $role->name, $from == "edit" ? $user->hasRole($role->name) : null ) }} {{ $role->display_name }}<br>
                     </div>
                 @endforeach
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="form-row col-md-6 clearfix">
+            <div class="form-col-1">
+                <label class="label">Name </label>
+            </div>
+            <div class="p-r-5 input-wrapper right">
+                {!! Form::text('first_name', $from=="edit" ? $user->person->first_name : null,['class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="form-row col-md-6 clearfix">
+            <div class="form-col-1">
+                <label class="label">Last Name </label>
+            </div>
+            <div class="p-r-5 input-wrapper right">
+                {!! Form::text('last_name', $from=="edit" ? $user->person->last_name : null,['class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="form-row col-md-6 clearfix">
+            <div class="form-col-1">
+                <label class="label">Username </label>
+            </div>
+            <div class="p-r-5 input-wrapper right">
+                {!! Form::text('username', $from=="edit" ? $user->username : null,['class' => 'form-control']) !!}
+            </div>
+        </div>
+        <div class="form-row col-md-6 clearfix">
+            <div class="form-col-1">
+                <label class="label">Created at </label>
+            </div>
+            <div class="p-r-5 input-wrapper right">
+                <div class='input-group date js-form-datetimepicker'>
+                    {!! Form::text('banned_at', $from=="edit" ? Carbon\Carbon::parse($user->banned_at)->format('d/m/Y h:i A') : null,
+                    ['class' => 'form-control', 'id' => 'banned_at', 'readonly' => 'readonly']) !!}
+                    <span class="input-group-addon">
+                        <span class="glyphicon glyphicon-calendar"></span>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
